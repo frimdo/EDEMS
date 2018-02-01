d1ebd59a99a1a58f03f9dc9fe9fc03bf146e1356
Dominik Friml <frimldom@gmail.cz>
2018-02-01 16:05:23 +0100

    load and store memory to/from localstorage

--- EDEMS/main.js
+++ EDEMS/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
 })
 
-var memory = new Array(65536);
-
-var registerA = new register(0);
-var registerB = new register(0);
-var registerC = new register(0);
