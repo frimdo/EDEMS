ebb71f41aaf8c3b0474d64eb48b166f5e3da7900
Dominik Friml <frimldom@gmail.com>
2018-03-28 16:22:09 +0200

    creation of light release version

--- EDEMS/js/microcodeCompiler.test.js
+++
@@ -1,8 +0,0 @@
-const uComp = require('./microcodeCompiler.js')
-
-test('compile', function () {
-  var input = `ALU 0x12
-R>DB 0xA
-INCW 0x1`
-  expect(uComp.compile(input)).toBe(["ALU", "0x12", "R>DB", "0xA", "INCW", "0x1"]);
-})
