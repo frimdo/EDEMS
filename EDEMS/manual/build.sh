e2ec9b9310bce9f18f05179cd817054ca8b0b870
Dominik Friml <frimldom@gmail.com>
2018-04-24 20:38:47 +0200

    README added

--- EDEMS/manual/build.sh
+++ EDEMS/manual/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pandoc -f markdown -t html index.md > tmp.html  
+pandoc -f markdown -t html ../../README.md > tmp.html  
 cat header.html tmp.html footer.html > index.html 
 rm tmp.html
 sed -i 's/↕/\&#8597;/g' index.html
